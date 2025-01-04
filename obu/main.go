15cbd03b71b8067b26d494bf521b4a1693f8509a
princedraculla <amirtorkashvand56@gmail.com>
2025-01-04 12:21:15 +0330

    (obu): generating on-board unit

---
+++ obu/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type OBUData struct {
+	ObuID int     `json:"obuId"`
+	Lat   float64 `json:"lat"`
+	Long  float64 `json:"long"`
+}
+
+func genCordinate() float64 {
+	n := float64(rand.Intn(100) + 1)
+	f := rand.Float64()
+	return n + f
+}
+
+func gentLocation() (float64, float64) {
+	return genCordinate(), genCordinate()
+}
+
+func gentOBUIDs(num int) []int {
+	ids := make([]int, num)
+	for i := 0; i < num; i++ {
+		ids[i] = rand.Intn(99999)
+	}
+	return ids
+}
+
+func main() {
+	lat, long := gentLocation()
+	obuIds := gentOBUIDs(20)
+
+	for {
+		for i := 0; i < len(obuIds); i++ {
+			data := &OBUData{
+				ObuID: obuIds[i],
+				Lat:   lat,
+				Long:  long,
+			}
+			fmt.Println(data)
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
