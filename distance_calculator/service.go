93306b163581f2f9e012c00b5e3dcde400f8453c
princedraculla <amirtorkashvand56@gmail.com>
2025-01-06 19:29:27 +0330

    (service): calculating distance function

--- distance_calculator/service.go
+++ distance_calculator/service.go
@@ -1,18 +1,36 @@
 package main
 
-import "github/princedraculla/toll-calculation/types"
+import (
+	"github/princedraculla/toll-calculation/types"
+	"math"
+)
 
 type CalculateServicer interface {
 	CalculateDistance(types.OBUData) (float64, error)
 }
 
 type CalculateService struct {
+	points [][]float64
 }
 
 func NewCalculatorService() CalculateServicer {
-	return &CalculateService{}
+	return &CalculateService{
+		points: make([][]float64, 0),
+	}
 }
 
 func (cs *CalculateService) CalculateDistance(data types.OBUData) (float64, error) {
-	return 0.0, nil
+	distance := 0.0
+
+	if len(cs.points) > 0 {
+		prevPoint := cs.points[len(cs.points)-1]
+		distance = calculateDistance(prevPoint[0], prevPoint[1], data.Lat, data.Long)
+	}
+	cs.points = append(cs.points, []float64{data.Lat, data.Long})
+
+	return distance, nil
+}
+
+func calculateDistance(x1, x2, y1, y2 float64) float64 {
+	return math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
 }
