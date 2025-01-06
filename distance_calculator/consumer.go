64c13889c9e75a81bfd39b8439d6967cf6124004
princedraculla <amirtorkashvand56@gmail.com>
2025-01-06 13:27:32 +0330

    (consumer): abstract consumer

--- distance_calculator/consumer.go
+++ distance_calculator/consumer.go
@@ -1,1 +1,17 @@
 package main
+
+type DataConsumer interface {
+	ConsumerData() error
+}
+
+type KafkaConsumer struct {
+	Consumer DataConsumer
+}
+
+func NewKafkaConsumer() (DataConsumer, error) {
+	return nil, nil
+}
+
+func (c *KafkaConsumer) ConsumerData(data any) error {
+	return nil
+}
--- distance_calculator/middleware.go
+++ distance_calculator/middleware.go
@@ -1,1 +1,15 @@
 package main
+
+type LogMiddleWareConsumer struct {
+	next DataConsumer
+}
+
+func NewLogMiddleWareConsumer(next DataConsumer) *LogMiddleWareConsumer {
+	return &LogMiddleWareConsumer{
+		next: next,
+	}
+}
+
+func (lc *LogMiddleWareConsumer) ConsumerData(data any) error {
+	return nil
+}
