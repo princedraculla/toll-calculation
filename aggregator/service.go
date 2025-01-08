22ae0b3030e40f9bf57cd53b293fe8b60635996e
princedraculla <amirtorkashvand56@gmail.com>
2025-01-08 16:39:14 +0330

    feat(service): calculating invoice

--- aggregator/service.go
+++ aggregator/service.go
@@ -4,12 +4,16 @@ import (
 	"github/princedraculla/toll-calculation/types"
 )
 
+const BasePrice = 3.15
+
 type Aggregator interface {
 	AggregateDistance(types.Distance) error
+	CalculateInvoice(int) (*types.Invoice, error)
 }
 
 type Store interface {
 	Insert(types.Distance) error
+	Get(int) (float64, error)
 }
 
 type InvoiceAggregator struct {
@@ -24,4 +28,17 @@ func NewInvoiceAggregator(storage Store) Aggregator {
 
 func (i *InvoiceAggregator) AggregateDistance(distance types.Distance) error {
 	return i.store.Insert(distance)
+}
+
+func (i *InvoiceAggregator) CalculateInvoice(id int) (*types.Invoice, error) {
+	dist, err := i.store.Get(id)
+	if err != nil {
+		return nil, err
+	}
+	inv := &types.Invoice{
+		OBUID:         id,
+		TotalDistance: dist,
+		TotalAmount:   BasePrice * dist,
+	}
+	return inv, nil
 }
